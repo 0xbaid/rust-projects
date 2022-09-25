5df123d5a5d90e0e6201d12627319f518bd1f638
0xbaid <obaidulhaq20@gmail.com>
2022-09-25 17:12:59 +0500

    completd guessing game from The Book

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,37 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Less => println!("{}", "Too small".red()),
+        };
+    }
 }
