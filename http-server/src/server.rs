26c49166a8ef8c80f13e43838ac160047f95b835
0xbaid <obaidulhaq20@gmail.com>
2022-10-09 19:17:02 +0500

    starts listening by taking buffer in bytes

--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 pub struct Server {
     addr: String,
@@ -16,6 +17,21 @@ impl Server {
     //on which the method is called on
     pub fn run(&self) {
         println!("Listening to {}", self.addr);
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recived a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection"),
+            }
+        }
     }
 }
