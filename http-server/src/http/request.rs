47ba6f8c2bc128f2be01c0c04372e550d87f4fd2
0xbaid <obaidulhaq20@gmail.com>
2022-10-11 23:46:48 +0500

    implemented from trait to convert errors

--- http-server/src/http/request.rs
+++ http-server/src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 pub struct Request {
     path: String,
     query_string: Option<String>, //wrapping query_string in option enum to handle if there's no value passed
@@ -11,8 +13,14 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
+    //converting byte slice to string slice
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!()
+        // let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+        //above we convert error to parse error, other way is to directly use ? and imp From for parse error
+        // as ? will try to convert utf8Error in to parseError by from trait implementation
+        let request = str::from_utf8(buf)?;
+
+        unimplemented!();
     }
 }
 
@@ -42,6 +50,12 @@ impl Display for ParseError {
 impl Debug for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        ParseError::InvalidEncoding
     }
 }
 
