36962b7e58910320690463249984c1cf3e8ff38a
0xbaid <obaidulhaq20@gmail.com>
2022-10-16 00:12:28 +0500

    parse string into parts

--- http-server/src/http/method.rs
+++ http-server/src/http/method.rs
@@ -1,4 +1,4 @@
-
+use std::str::FromStr;
 pub enum Method {
     GET,
     HEAD,
@@ -10,3 +10,25 @@ pub enum Method {
     TRACE,
     PATCH,
 }
+
+//FromStr parse a value from string
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "HEAD" => Ok(Self::HEAD),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "DELETE" => Ok(Self::DELETE),
+            "CONNECT" => Ok(Self::CONNECT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
+            _ => Err(MethodError),
+        }
+    }
+}
+
+pub struct MethodError;
--- http-server/src/http/request.rs
+++ http-server/src/http/request.rs
@@ -1,27 +1,69 @@
-use super::method::Method;
+use crate::http::request;
+
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str;
 use std::str::Utf8Error;
-pub struct Request {
-    path: String,
-    query_string: Option<String>, //wrapping query_string in option enum to handle if there's no value passed
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>, //wrapping query_string in option enum to handle if there's no value passed
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
+    //GET /search?name=abc&sort=1 HTTP/1.1
     //converting byte slice to string slice
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         // let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
         //above we convert error to parse error, other way is to directly use ? and imp From for parse error
         // as ? will try to convert utf8Error in to parseError by from trait implementation
         let request = str::from_utf8(buf)?;
 
-        unimplemented!();
+        //variable shadowing, overwriting variable request with the remaining part
+        //after extracting parts from url on the basis of space
+        //every we create new tuple with request, old value of request become unusable
+        //ok_or turns option enum to result enum, on success wraps value in ok(()) otherwise returns
+        //error which is passed in ok_or
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        //parse comes where we implemented from_str trait
+        //our function returns parse error but parse returns method error
+        //we need to convert it using From trait
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        //use if let statement becasue we care about the some variant
+        //if let matches the pattern unwraps the result
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i]
+        }
+        Ok(Self {
+            path,
+            query_string,
+            method,
+        })
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+    None
 }
 
 pub enum ParseError {
@@ -50,6 +92,12 @@ impl Display for ParseError {
 impl Debug for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        ParseError::InvalidMethod
     }
 }
 
